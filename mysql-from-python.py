32294d25973fc078a39e87584ed20e04508c0a43
Mike <mlr314159@yahoo.co.uk>
2020-04-09 13:56:36 +0000

    Initial commit

---
+++ mysql-from-python.py
@@ -0,0 +1,23 @@
+import os
+import pymysql
+
+# get username
+# (modify this variable if runn ing on a different environment)
+username = os.getenv('C9_USER')
+
+connection = pymysql.connect(host='localhost',
+                            user=username,
+                            password='',
+                            db='Chinook')
+
+try:
+    #  Run a query       
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    # Close the connection. regardless of whether the above was successfull
+    connection.close()
+
